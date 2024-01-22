f4bde95d0d8487a5f9c2355217f0d01d2c4fa4ea
KomisarzRyba <antek.olesik@gmail.com>
2024-01-22 15:19:45 -0500

    feat: whitespace, punctuator lex

--- src/ast/lexer.rs
+++ src/ast/lexer.rs
@@ -1,3 +1,5 @@
+use std::{char, os, str::Chars};
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum TokenKind {
     BadToken,
@@ -59,25 +61,28 @@ impl<'a> Lexer<'a> {
         self.input.chars().nth(self.cur_pos).unwrap()
     }
 
-    fn consume(&mut self) -> Option<char> {
+    fn peek(&self) -> Option<char> {
         if self.cur_pos >= self.input.len() {
             return None;
         }
         let c = self.cur_char();
-        self.cur_pos += 1;
         Some(c)
+    }
+
+    fn next_pos(&mut self) {
+        self.cur_pos += 1;
     }
 
     fn consume_number(&mut self) -> i64 {
         let mut num = 0i64;
-        while let Some(c) = self.consume() {
+        while let Some(c) = self.peek() {
             let c = c.to_digit(10);
             match c {
                 Some(d) => {
                     num = num * 10 + d as i64;
+                    self.next_pos();
                 }
                 None => {
-                    self.cur_pos -= 1;
                     break;
                 }
             }
@@ -85,24 +90,39 @@ impl<'a> Lexer<'a> {
         num
     }
 
-    fn next(&mut self) -> Option<Token> {
-        if self.cur_pos > self.input.len() {
-            return None;
+    fn consume_whitespace(&mut self) {
+        while let Some(c) = self.peek() {
+            if c.is_ascii_whitespace() {
+                self.next_pos();
+            } else {
+                break;
+            }
         }
-        if self.cur_pos == self.input.len() {
-            return Some(Token::new(
-                TokenKind::EOF,
-                TextSpan::new(0, 0, '\0'.to_string()),
-            ));
-        }
+    }
 
-        let start = self.cur_pos;
+    fn consume_punctuator(&mut self) -> TokenKind {
         let c = self.cur_char();
-        let mut kind = TokenKind::BadToken;
+        self.next_pos();
+        match c {
+            '+' => TokenKind::Plus,
+            // add more
+            _ => TokenKind::BadToken,
+        }
+    }
 
+    fn consume(&mut self) -> Option<Token> {
+        let start = self.cur_pos;
+        let mut kind = TokenKind::BadToken;
+        let c = self.cur_char();
         if c.is_digit(10) {
-            let number = self.consume_number();
-            kind = TokenKind::Number(number)
+            kind = TokenKind::Number(self.consume_number());
+        } else if c.is_ascii_whitespace() {
+            self.consume_whitespace();
+            kind = TokenKind::Whitespace;
+        } else if c.is_ascii_punctuation() {
+            kind = self.consume_punctuator();
+        } else {
+            self.next_pos();
         }
 
         let end = self.cur_pos;
@@ -110,6 +130,22 @@ impl<'a> Lexer<'a> {
         let span = TextSpan::new(start, end, literal);
 
         Some(Token::new(kind, span))
+    }
+
+    fn next(&mut self) -> Option<Token> {
+        if self.cur_pos > self.input.len() {
+            return None;
+        }
+        if self.cur_pos == self.input.len() {
+            let t = Token::new(
+                TokenKind::EOF,
+                TextSpan::new(self.cur_pos, self.cur_pos, '\0'.to_string()),
+            );
+            self.next_pos();
+            return Some(t);
+        }
+
+        self.consume()
     }
 }
 
@@ -129,9 +165,30 @@ mod tests {
         let path = get_path();
         let content = &fs::read_to_string(path)?;
         let mut l = Lexer::new(content);
+        // let mut tokens: Vec<Token> = Vec::new();
+        // while let Some(t) = l.next() {
+        //     tokens.push(t);
+        // }
+        // println!("{:?}", tokens);
         assert_eq!(
             l.next().unwrap(),
             Token::new(TokenKind::Number(12), TextSpan::new(0, 2, "12".to_string()))
+        );
+        assert_eq!(
+            l.next().unwrap(),
+            Token::new(TokenKind::Whitespace, TextSpan::new(2, 3, ' '.to_string()))
+        );
+        assert_eq!(
+            l.next().unwrap(),
+            Token::new(TokenKind::Plus, TextSpan::new(3, 4, "+".to_string()))
+        );
+        assert_eq!(
+            l.next().unwrap(),
+            Token::new(TokenKind::Whitespace, TextSpan::new(4, 5, '\n'.to_string()))
+        );
+        assert_eq!(
+            l.next().unwrap(),
+            Token::new(TokenKind::EOF, TextSpan::new(5, 5, '\0'.to_string()))
         );
         Ok(())
     }
