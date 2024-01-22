34b67fa72af43dd218f5a3151082a12161e13755
KomisarzRyba <antek.olesik@gmail.com>
2024-01-21 21:38:18 -0500

    feat: lexing numbers

--- src/ast/lexer.rs
+++ src/ast/lexer.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq, Eq)]
 pub enum TokenKind {
     BadToken,
     EOF,
@@ -11,6 +12,7 @@ pub enum TokenKind {
     RightParen,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct TextSpan {
     start: usize,
     end: usize,
@@ -31,6 +33,7 @@ impl TextSpan {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Token {
     kind: TokenKind,
     span: TextSpan,
@@ -57,16 +60,29 @@ impl<'a> Lexer<'a> {
     }
 
     fn consume(&mut self) -> Option<char> {
-        let c = self.cur_char();
-        self.cur_pos += 1;
         if self.cur_pos >= self.input.len() {
             return None;
         }
+        let c = self.cur_char();
+        self.cur_pos += 1;
         Some(c)
     }
 
-    fn is_number_start(c: &char) -> bool {
-        c.is_digit(10)
+    fn consume_number(&mut self) -> i64 {
+        let mut num = 0i64;
+        while let Some(c) = self.consume() {
+            let c = c.to_digit(10);
+            match c {
+                Some(d) => {
+                    num = num * 10 + d as i64;
+                }
+                None => {
+                    self.cur_pos -= 1;
+                    break;
+                }
+            }
+        }
+        num
     }
 
     fn next(&mut self) -> Option<Token> {
@@ -84,8 +100,8 @@ impl<'a> Lexer<'a> {
         let c = self.cur_char();
         let mut kind = TokenKind::BadToken;
 
-        if Self::is_number_start(&c) {
-            let number: i64 = Self::tokenize_number(&c);
+        if c.is_digit(10) {
+            let number = self.consume_number();
             kind = TokenKind::Number(number)
         }
 
@@ -94,5 +110,29 @@ impl<'a> Lexer<'a> {
         let span = TextSpan::new(start, end, literal);
 
         Some(Token::new(kind, span))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{env, fs};
+
+    use super::*;
+
+    fn get_path() -> String {
+        let args: Vec<String> = env::args().collect();
+        args[2].to_owned()
+    }
+
+    #[test]
+    fn lex_num() -> Result<(), Box<dyn std::error::Error>> {
+        let path = get_path();
+        let content = &fs::read_to_string(path)?;
+        let mut l = Lexer::new(content);
+        assert_eq!(
+            l.next().unwrap(),
+            Token::new(TokenKind::Number(12), TextSpan::new(0, 2, "12".to_string()))
+        );
+        Ok(())
     }
 }
